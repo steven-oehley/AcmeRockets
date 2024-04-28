9c308055bc851200d25cdebdefc517249c1feb66
Steve <steve.gitprofile@outlook.com>
2024-04-28 12:31:40 +0200

    added animation for mobile nav

--- build/js/index.js
+++ build/js/index.js
@@ -4,13 +4,21 @@ const hamburgerMenuBtn = document.querySelector('#hamburger-btn');
 const mobileNavExitBtn = document.querySelector('#exit-btn');
 const mobileMenuEl = document.querySelector('#mobile-menu');
 
+const showClassAnimations = [
+  'animate-fade-left',
+  'animate-once',
+  'animate-ease-in-out',
+];
+
 hamburgerMenuBtn.addEventListener('click', () => {
   mobileMenuEl.classList.remove('hidden');
+  mobileMenuEl.classList.add('flex', ...showClassAnimations);
 });
 
 mobileMenuEl.addEventListener('click', (e) => {
   // gaurd  clause
   if (e.target.tagName !== 'A' && e.target.tagName !== 'BUTTON') return;
 
+  mobileMenuEl.classList.remove('flex');
   mobileMenuEl.classList.add('hidden');
 });
